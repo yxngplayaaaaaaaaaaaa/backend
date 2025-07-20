a4733b8bd71b69f0e17dbce100f1d3f320d14d0b
yxngplayaaaaaaaaaaaa <ivnpaypal2005@gmail.com>
2025-07-20 23:23:10 +0200

    Update verify.js

--- api/verify.js
+++ api/verify.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://legiixnutpcnmleewqqj.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'; // use your full anon key here
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxlZ2lpeG51dHBjbm1sZWV3cXFqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMwNDI0ODAsImV4cCI6MjA2ODYxODQ4MH0.sE6VDWCoh5lpWDQNBxvOk-Jg9NyDkaWTQ02qb7m8k1k'; // use your full anon key here
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 function xorEncrypt(data, key) {
